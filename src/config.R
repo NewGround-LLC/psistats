9c3a7520089e37953a8fae8dd341927f61f7fc80
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-01-20 18:30:43 +0200

    The paths to data directories moved into global configuration

---
+++ src/config.R
@@ -0,0 +1,23 @@
+# The global configuration holder
+#
+
+#
+# The directory to load data sets from
+input_data_dir <- "DataSets/sample_dataset"
+# The input users CSV data file
+input_users_csv <- sprintf("%s/users.csv", input_data_dir)
+# The input likes CSV data file
+input_likes_csv <- sprintf("%s/likes.csv", input_data_dir)
+# The input users-likes CSV data file
+input_ul_csv <- sprintf("%s/users-likes.csv", input_data_dir)
+
+#
+# The directory to hold outputs
+out_dir <- "out"
+# The directory to hold preprocessed outputs
+out_intermediates_dir <- sprintf("%s/intermediate", out_dir)
+
+# The intermediate file to hold preprocessed users data
+users_prdata_file <- sprintf("%s/users.RData", out_intermediates_dir)
+# The intermediate file to store preprocessed Users-Likes sparse matrix
+ul_prdata_file <- sprintf("%s/M.RData", out_intermediates_dir)
