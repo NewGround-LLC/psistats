403481dfa881ac16c57c6cbef4eeb5da7b0adbd8
Iaroslav Omelianenko <yaric.mail@gmail.com>
2017-01-22 20:04:25 +0200

    Removed cross-validation folding from SVD correlation analysis - it has no sense there.

--- src/analysis.R
+++ src/analysis.R
@@ -1,7 +1,6 @@
 # This is preliminary analysis to find correlations between users scores on varimax rotated SVD dimensions 
-# and users' psychodemographic traits. The matrix with users scores on varimax rotated SVD dimensions
-# generated during analysis will be saved and can be used to build corresponding ML models in other
-# modules.
+# and users' psychodemographic traits. As results of analysis heatmap will be generated as visual representation
+# of mentioned correlation.
 
 source('./src/config.R')
 
@@ -20,26 +19,18 @@ cat("Users:\t\t", dim(users), "\n")
 load(ul_prdata_file)
 cat("Users-Likes:\t\t", dim(M), "\n")
 
-#
-# Prepare samples for k-fold cross-validation 
-folds <- sample(1:10, size = nrow(users), replace = TRUE)
-# set appart test data samples
-test <- folds == 1
+# set random number generator seed - so results will be stable from run to run
+set.seed(44)
 
 #
 # Perform dimensionality reduction with SVD for 50 dimensions
-K <- 50
-Msvd <- irlba(M[!test,], nv = K)
+Msvd <- irlba(M, nv = K)
 # Get varimax rotated scores for Likes per SVD dimensions
 likesSVDrot <- unclass(varimax(Msvd$v)$loadings)
 # Get varimax rotated scores for Users per SVD dimensions
 usersSVDrot <- as.data.frame(as.matrix(M %*% likesSVDrot))
 
 print("Dimensionality reduction complete")
-
-# save calculated users scores and cross-validation folds
-save(usersSVDrot, file = users_rot_varimax_file)
-save(folds, file = users_cv_folds_file)
 
 # find correlations between users scores on varimax rotated SVD dimensions and users psychodemographic traits
 users_svd_corr <- cor(usersSVDrot, users[, -1], use = "pairwise.complete.obs")
