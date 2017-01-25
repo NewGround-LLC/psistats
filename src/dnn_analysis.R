e53a51c0db0d3a2bfc48cb6b626c65151542f63e
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-01-25 19:29:49 +0200

    Implemented training routines for model

--- src/dnn_analysis.R
+++ src/dnn_analysis.R
@@ -17,29 +17,8 @@ flags$DEFINE_integer('batch_size', 100L, 'Batch size. Must divide evenly into th
 flags$DEFINE_string('train_dir', sprintf("%s/train_data", out_intermediates_dir), 'Directory to put the training data.')
 FLAGS <- parse_flags()
 
-# Check that input data exist
-print("Checking that input data files exist")
-assertthat::assert_that(file.exists(users_prdata_file))
-assertthat::assert_that(file.exists(ul_prdata_file))
-
-# 
-# Load data
-input_data = ULDataSet$new()
-
 # set random number generator's seed - so results will be stable from run to run
 set.seed(44)
-
-# Returns next random batch from data set
-#
-# Args:
-#   batch_size: The batch size will be baked into both placeholders.
-#
-# Returns:
-#   batch$user_likes: The list of train data inputs
-#   batch$user_traits: the list of train ground truth data
-next_batch <- function(batch_size) {
-  stop("Needed definition of method here")
-}
 
 
 # Generate placeholder variables to represent the input tensors.
@@ -51,19 +30,19 @@ next_batch <- function(batch_size) {
 #   batch_size: The batch size will be baked into both placeholders.
 #
 # Returns:
-#   placeholders$user_likes: the Users-Likes placeholder.
-#   placeholders$user_traits: the user traits placeholder.
+#   placeholders$users_likes: the Users-Likes placeholder.
+#   placeholders$users: the user traits placeholder.
 #
 placeholder_inputs <- function(batch_size, features_dimensions) {
   
   # Note that the shapes of the placeholders match the shapes of the full
   # User-Likes and user traits tensors, except the first dimension is now batch_size
   # rather than the full size of the train or test data sets.
-  user_likes <- tf$placeholder(tf$float32, shape(batch_size, features_dimensions))
+  users_likes <- tf$placeholder(tf$float32, shape(batch_size, features_dimensions))
   user_traits <- tf$placeholder(tf$int32, shape(batch_size, OUTPUTS_DIMENSION))
   
   # return both placeholders
-  list(user_likes = user_likes, user_traits = user_traits)
+  list(users_likes = users_likes, users = user_traits)
 }
 
 # Fills the feed_dict for training the given step.
@@ -75,27 +54,112 @@ placeholder_inputs <- function(batch_size, features_dimensions) {
 #   )
 #
 # Args:
-#   data_set: The set of user likes and user traits, from input_data.read_data_sets()
-#   user_likes_pl: the Users-Likes placeholder, from placeholder_inputs().
-#   user_traits_pl: the user traits placeholder, from placeholder_inputs().
+#   data_set: The set of user likes and user traits, from ul.read_data_sets()
+#   users_likes_pl: the Users-Likes placeholder, from placeholder_inputs().
+#   users_pl: the users traits placeholder, from placeholder_inputs().
 #
 # Returns:
 #   feed_dict: The feed dictionary mapping from placeholders to values.
 #
-fill_feed_dict <- function(data_set, user_likes_pl, user_traits_pl) {
+fill_feed_dict <- function(data_set, users_likes_pl, users_pl) {
   # Create the feed_dict for the placeholders filled with the next
   # `batch size` examples.
   batch <- data_set$next_batch(FLAGS$batch_size)
+  users_preprocessed = batch$users[,-1] # removing userid - it has unique value
+  users_preprocessed['age'] <- scale(batch$users['age']) # scale and center AGE to avoid bias in RELU
   dict(
-    user_likes_pl = batch$user_likes,
-    user_traits_pl = batch$user_traits
+    users_likes_pl = batch$users_likes,
+    users_pl = users_preprocessed
   )
 }
 
 # Train users psychometric model
 
+# Check that input data exist
+print("Checking that input data files exist")
+assertthat::assert_that(file.exists(users_prdata_file))
+assertthat::assert_that(file.exists(ul_prdata_file))
+
 # Get sets of users-likes and users traits for train and test
-data_sets <- input_data$read_data_set(ul_file = ul_prdata_file, users_file = users_prdata_file)
+data_sets <- ul.read_data_set(ul_file = ul_prdata_file, users_file = users_prdata_file)
+
+# Tell TensorFlow that the model will be built into the default Graph.
+with(tf$Graph()$as_default(), {
+  
+  # Generate placeholders for the users-likes and users
+  placeholders <- placeholder_inputs(FLAGS$batch_size, data_sets$features_dimension)
+  
+  # Build a Graph that computes predictions from the inference model.
+  predicts <- inference(placeholders$users_likes, FLAGS$hidden1, FLAGS$hidden2)
+  
+  # Add to the Graph the Ops for loss calculation.
+  loss <- loss(predicts, placeholders$users)
+  
+  # Add to the Graph the Ops that calculate and apply gradients.
+  train_op <- training(loss, FLAGS$learning_rate)
+  
+  # Add the Op to compare the predictions to the ground truth during evaluation.
+  #eval_correct <- evaluation(predicts, placeholders$users)
+  
+  # Build the summary Tensor based on the TF collection of Summaries.
+  summary <- tf$summary$merge_all()
+  
+  # Add the variable initializer Op.
+  init <- tf$global_variables_initializer()
+  
+  # Create a saver for writing training checkpoints.
+  saver <- tf$train$Saver()
+  
+  # Create a session for running Ops on the Graph.
+  sess <- tf$Session()
+  
+  # Instantiate a SummaryWriter to output summaries and the Graph.
+  summary_writer <- tf$summary$FileWriter(FLAGS$train_dir, sess$graph)
+  
+  # And then after everything is built:
+  
+  # Run the Op to initialize the variables.
+  sess$run(init)
+  
+  # Start the training loop.
+  for (step in 1:FLAGS$max_steps) {
+    start_time <- Sys.time()
+    
+    # Fill a feed dictionary with the actual set of users-likes and users
+    # for this particular training step.
+    feed_dict <- fill_feed_dict(data_set = data_sets$train,
+                                users_likes_pl = placeholders$users_likes,
+                                users_pl = placeholders$users)
+    
+    # Run one step of the model.  The return values are the activations
+    # from the `train_op` (which is discarded) and the `loss` Op.  To
+    # inspect the values of your Ops or variables, you may include them
+    # in the list passed to sess.run() and the value tensors will be
+    # returned in the tuple from the call.
+    values <- sess$run(list(train_op, loss), feed_dict = feed_dict)
+    loss_value <- values[[2]]
+    
+    duration <- Sys.time() - start_time
+    
+    # Write the summaries and print an overview fairly often.
+    if (step %% 100 == 0) {
+      # Print status to stdout.
+      cat(sprintf('Step %d: loss = %.2f (%.3f sec)\n',
+                  step, loss_value, duration))
+      # Update the events file.
+      summary_str <- sess$run(summary, feed_dict = feed_dict)
+      summary_writer$add_summary(summary_str, step)
+      summary_writer$flush()
+    }
+    
+    # Save a checkpoint and evaluate the model periodically.
+    if ((step + 1) %% 1000 == 0 || (step + 1) == FLAGS$max_steps) {
+      checkpoint_file <- file.path(FLAGS$train_dir, 'checkpoint')
+      saver$save(sess, checkpoint_file, global_step = step)
+      
+    }
+  }
+})
 
 
 
