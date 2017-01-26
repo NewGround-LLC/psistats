71c3f4844863fd58668488cd81973ea515d0c7cc
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-01-26 17:26:58 +0200

    Implemented data set preprocessing routines

--- src/users_likes_data_set.R
+++ src/users_likes_data_set.R
@@ -3,40 +3,27 @@
 library(R6)
 library(irlba)
 
-# The data set factory to load and generate data set from provided files
+# The function to read preprocessed data set from saved SVD Users-Likes matrix and Users traits list.
 #
 # Args:
 #   ul_file: the file with Users-Likes sparse matrix
 #   users_file: the file with Users traits table
-#   with_svd: the flag to indicate if SVD should be applied to reduce features dimension [default: false]
-#   svd_k: the number of SVD dimensions [default: 50]
 # Returns:
 #   list comprising of two data sets (train, test) and number of features in data samples (features_dimension)
-ul.read_data_set <- function(ul_file, users_file, with_svd = FALSE, svk_k = 50) {
+ul.read_data_set <- function(ul_file, users_file) {
   load(users_prdata_file)
   cat(sprintf("%12s : [%d, %d]\n","Users", dim(users)[1], dim(users)[2]))
-  load(ul_prdata_file)
+  load(ul_file)
   cat(sprintf("%12s : [%d, %d]\n", "Users-Likes", dim(M)[1], dim(M)[2]))
   
   # prepare data
   folds <- sample(1:n_folds, size = nrow(users), replace = TRUE) # the data samples folds for cross-validation
   test <- folds == 1
   
-  # Apply SVD if appropriate
-  if(with_svd) {
-    # do SVD rotation
-    Msvd <- irlba(M[!test, ], nv = svk_k)
-    likesSVDrot <- unclass(varimax(Msvd$v)$loadings) # varimax rotated likes per SVD dimensions
-    M <- as.data.frame(as.matrix(M %*% likesSVDrot)) # the varimax rotated users-likes per SVD dimensions
-  }
-
   # create train/test data sets
   train <- ULDataSet$new(users = users[!test,], users_likes = M[!test,])
   test <- ULDataSet$new(users = users[test,], users_likes = M[test,])
   features_dimension <- dim(M)[2]
-  
-  # release memory
-  rm(M, users)
   
   # create list and return
   list(
@@ -44,6 +31,30 @@ ul.read_data_set <- function(ul_file, users_file, with_svd = FALSE, svk_k = 50) 
     test = test,
     features_dimension = features_dimension
   )
+}
+
+# The preprocessing function to reduce features dimension with SVD in Users-Likes matrix and
+# save resulting matrix for further use.
+#
+# Args:
+#   ul_file: the file with full features sparse Users-Likes matrix
+#   out_file: the file to store preprocessing results
+#   svd_k: the number of SVD dimensions [default: 50]
+ul.save_features_reduced_data_set(ul_file, out_file, svd_k = 50) {
+  load(ul_prdata_file)
+  cat(sprintf("%12s : [%d, %d]\n", "Users-Likes", dim(M)[1], dim(M)[2]))
+  
+  # do SVD with varimax rotation
+  start_time <- Sys.time()
+  Msvd <- irlba(M, nv = svd_k)
+  likesSVDrot <- unclass(varimax(Msvd$v)$loadings) # varimax rotated likes per SVD dimensions
+  M <- as.data.frame(as.matrix(M %*% likesSVDrot)) # the varimax rotated users-likes per SVD dimensions
+  duration <- Sys.time() - start_time
+  # save reduced matrix if requested
+  save(M, file = out_file)
+  
+  cat(sprintf('SVD with varimax rotation complete, dimensions: %d, duration: %s, resulting file: %s\n',
+              svd_k, duration, out_file))
 }
 
 # The Users-Likes psychodemographics data set holder class
