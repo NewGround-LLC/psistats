8b49634c2f834172a423ff073a3f5ec1251a9d1d
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-01-26 11:17:45 +0200

    Renamed features and labels placeholders in order to make it clear what is what

--- src/dnn_analysis.R
+++ src/dnn_analysis.R
@@ -30,19 +30,19 @@ set.seed(44)
 #   batch_size: The batch size will be baked into both placeholders.
 #
 # Returns:
-#   placeholders$users_likes: the Users-Likes placeholder.
-#   placeholders$users: the user traits placeholder.
+#   placeholders$features: the Users-Likes placeholder.
+#   placeholders$labels: the user traits placeholder.
 #
 placeholder_inputs <- function(batch_size, features_dimensions) {
   
   # Note that the shapes of the placeholders match the shapes of the full
   # User-Likes and user traits tensors, except the first dimension is now batch_size
   # rather than the full size of the train or test data sets.
-  users_likes <- tf$placeholder(tf$float32, shape(batch_size, features_dimensions))
+  features <- tf$placeholder(tf$float32, shape(batch_size, features_dimensions))
   user_traits <- tf$placeholder(tf$int32, shape(batch_size, OUTPUTS_DIMENSION))
   
   # return both placeholders
-  list(users_likes = users_likes, users = user_traits)
+  list(features = features, labels = user_traits)
 }
 
 # Fills the feed_dict for training the given step.
@@ -55,21 +55,21 @@ placeholder_inputs <- function(batch_size, features_dimensions) {
 #
 # Args:
 #   data_set: The set of user likes and user traits, from ul.read_data_sets()
-#   users_likes_pl: the Users-Likes placeholder, from placeholder_inputs().
-#   users_pl: the users traits placeholder, from placeholder_inputs().
+#   features_pl: the Users-Likes placeholder, from placeholder_inputs().
+#   labels_pl: the users traits placeholder, from placeholder_inputs().
 #
 # Returns:
 #   feed_dict: The feed dictionary mapping from placeholders to values.
 #
-fill_feed_dict <- function(data_set, users_likes_pl, users_pl) {
+fill_feed_dict <- function(data_set, features_pl, labels_pl) {
   # Create the feed_dict for the placeholders filled with the next
   # `batch size` examples.
   batch <- data_set$next_batch(FLAGS$batch_size)
   users_preprocessed = batch$users[,-1] # removing userid - it has unique value
-  users_preprocessed['age'] <- scale(batch$users['age']) # scale and center AGE to avoid bias in RELU
+  #users_preprocessed['age'] <- scale(batch$users['age']) # scale and center AGE
   dict(
-    users_likes_pl = batch$users_likes,
-    users_pl = users_preprocessed
+    features_pl = batch$users_likes,
+    labels_pl = users_preprocessed
   )
 }
 
@@ -90,16 +90,16 @@ with(tf$Graph()$as_default(), {
   placeholders <- placeholder_inputs(FLAGS$batch_size, data_sets$features_dimension)
   
   # Build a Graph that computes predictions from the inference model.
-  predicts <- inference(placeholders$users_likes, FLAGS$hidden1, FLAGS$hidden2)
+  predicts <- inference(placeholders$features, FLAGS$hidden1, FLAGS$hidden2)
   
   # Add to the Graph the Ops for loss calculation.
-  loss <- loss(predicts, placeholders$users)
+  loss <- loss(predicts, placeholders$labels)
   
   # Add to the Graph the Ops that calculate and apply gradients.
   train_op <- training(loss, FLAGS$learning_rate)
   
   # Add the Op to compare the predictions to the ground truth during evaluation.
-  #eval_correct <- evaluation(predicts, placeholders$users)
+  #eval_correct <- evaluation(predicts, placeholders$labels)
   
   # Build the summary Tensor based on the TF collection of Summaries.
   summary <- tf$summary$merge_all()
@@ -128,8 +128,8 @@ with(tf$Graph()$as_default(), {
     # Fill a feed dictionary with the actual set of users-likes and users
     # for this particular training step.
     feed_dict <- fill_feed_dict(data_set = data_sets$train,
-                                users_likes_pl = placeholders$users_likes,
-                                users_pl = placeholders$users)
+                                features_pl = placeholders$features,
+                                labels_pl = placeholders$labels)
     
     # Run one step of the model.  The return values are the activations
     # from the `train_op` (which is discarded) and the `loss` Op.  To
