07f9c70673f5c1a4c12c210f47069a713de50923
Iaroslav Omelianenko <yaric.mail@gmail.com>
2017-01-30 22:47:39 +0200

    The command line routines aimed to perform preprocessing using SVD with varimax moved to separate script.

--- src/users_likes_data_set.R
+++ src/users_likes_data_set.R
@@ -103,23 +103,3 @@ ULDataSet <- R6Class(
     users_likes = NA
   )
 ) 
-
-library(optparse)
-
-# parse command line arguments
-option_list <- list(
-  make_option(c("-k", "--svd_dimensions"), type="integer", default=50,
-              help="the number of SVD dimensions to apply when doing features dimensionality reduction [default %default]")
-)
-parser <- OptionParser(usage = "%prog [options] file", option_list = option_list, add_help_option = TRUE, 
-                       description = "This will reduce number of features dimensions by applying SVD.")
-args <- parse_args(parser, positional_arguments = TRUE)
-opt <- args$options
-print(sprintf("Number of SVD dimensions to apply: %d", opt$svd_dimensions))
-
-# Check that input data exist
-print("Checking that input data files exist")
-assertthat::assert_that(file.exists(ul_prdata_file))
-
-# run SVD 
-ul_save_features_reduced_data_set(ul_file = ul_prdata_file, out_file = ul_reduced_prdata_file, svd_k = opt$svd_dimensions)
