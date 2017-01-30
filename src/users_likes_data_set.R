09078233ae582e2e0f03197abb9ec7dab2567951
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-01-30 18:03:20 +0200

    Added command line invocation routines in order to preprocess data set with SVD varimax rotation dimensionality reduction

--- src/users_likes_data_set.R
+++ src/users_likes_data_set.R
@@ -3,6 +3,8 @@
 library(R6)
 library(irlba)
 
+source('./src/config.R')
+
 # The function to read preprocessed data set from saved SVD Users-Likes matrix and Users traits list.
 #
 # Args:
@@ -101,3 +103,23 @@ ULDataSet <- R6Class(
     users_likes = NA
   )
 ) 
+
+library(optparse)
+
+# parse command line arguments
+option_list <- list(
+  make_option(c("-k", "--svd_dimensions"), type="integer", default=50,
+              help="the number of SVD dimensions to apply when doing features dimensionality reduction [default %default]")
+)
+parser <- OptionParser(usage = "%prog [options] file", option_list = option_list, add_help_option = TRUE, 
+                       description = "This will reduce number of features dimensions by applying SVD.")
+args <- parse_args(parser, positional_arguments = TRUE)
+opt <- args$options
+print(sprintf("Number of SVD dimensions to apply: %d", opt$svd_dimensions))
+
+# Check that input data exist
+print("Checking that input data files exist")
+assertthat::assert_that(file.exists(ul_prdata_file))
+
+# run SVD 
+ul_save_features_reduced_data_set(ul_file = ul_prdata_file, out_file = ul_reduced_prdata_file, svd_k = opt$svd_dimensions)
