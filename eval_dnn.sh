6022ff48544c555991afe20ff52bab3013b36aae
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-02-10 17:33:42 +0200

    Added additional parameters for number of neurons in second layer

--- eval_dnn.sh
+++ eval_dnn.sh
@@ -6,6 +6,7 @@
 ~/anaconda3/bin/Rscript src/nn_analysis.R --learning_rate 0.001 \
                                           --max_steps 50000 \
                                           --hidden1 512 \
+                                          --hidden2 256 \
                                           --batch_size 100 \
                                           --dropout 0.5 \
                                           --lr_anneal_step 10000 \
