cd4966f6b8562767f053f9c7d883c63eb1b9f5b0
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-02-10 17:30:53 +0200

    Added first DNN implementation

---
+++ eval_dnn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# The evaluation script for experiment with Deep Learning Network
+#
+
+~/anaconda3/bin/Rscript src/nn_analysis.R --learning_rate 0.001 \
+                                          --max_steps 50000 \
+                                          --hidden1 512 \
+                                          --batch_size 100 \
+                                          --dropout 0.5 \
+                                          --lr_anneal_step 10000 \
+                                          --network_type dnn
