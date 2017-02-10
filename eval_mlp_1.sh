ef7be3b794e88bcbfaf71a16d77bb39717e4e96c
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-02-10 18:19:19 +0200

    Input parameters fixed to include layers

--- eval_mlp_1.sh
+++ eval_mlp_1.sh
@@ -3,4 +3,10 @@
 # The script to run multilayer perceptron with one hidden layer with specific
 # set of parameters
 
-~/anaconda3/bin/Rscript src/nn_analysis.R --learning_rate 0.001 --max_steps 50000 --hidden1 512 --batch_size 100 --dropout 0.5 --lr_anneal_step 10000 --network_type mlp
+~/anaconda3/bin/Rscript src/nn_analysis.R --learning_rate 0.001 \
+                                          --max_steps 50000 \
+                                          --layers 512 \
+                                          --batch_size 100 \
+                                          --dropout 0.5 \
+                                          --lr_anneal_step 10000 \
+                                          --network_type mlp
