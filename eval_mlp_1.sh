5ac63418ee55585862592b237c4d7b1378349114
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-02-15 18:34:43 +0200

    Added plots with results of two layer ann evaluation

--- eval_mlp_1.sh
+++ eval_mlp_1.sh
@@ -3,7 +3,7 @@
 # The script to run multilayer perceptron with one hidden layer with specific
 # set of parameters
 
-~/anaconda3/bin/Rscript src/nn_analysis.R --learning_rate 0.001 \
+~/anaconda3/bin/Rscript src/nn_analysis.R --learning_rate 0.00001 \
                                           --max_steps 50000 \
                                           --layers 512 \
                                           --batch_size 100 \
