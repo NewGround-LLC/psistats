badaf0961670e48e4f03137d8765ea515b6b572d
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-02-16 11:21:16 +0200

    Added command line option to enable/disable varimax rotation

--- src/svd_varimax.R
+++ src/svd_varimax.R
@@ -7,13 +7,15 @@ library(optparse)
 # parse command line arguments
 option_list <- list(
   make_option(c("-k", "--svd_dimensions"), type="integer", default=50,
-              help="the number of SVD dimensions to apply when doing features dimensionality reduction [default %default]")
+              help="the number of SVD dimensions to apply when doing features dimensionality reduction [default %default]"),
+  make_option(c("-v", "--apply_varimax"), type="logical", default=TRUE,
+              help="the flag to indicate whether varimax rotation should be applied to results of SVD factor analysis [default %default]")
 )
 parser <- OptionParser(usage = "%prog [options] file", option_list = option_list, add_help_option = TRUE, 
                        description = "This will reduce number of features dimensions by applying SVD.")
 args <- parse_args(parser, positional_arguments = TRUE)
 opt <- args$options
-print(sprintf("Number of SVD dimensions to apply: %d", opt$svd_dimensions))
+print(sprintf("Number of SVD dimensions to apply: %d, apply varimax: %s", opt$svd_dimensions, opt$apply_varimax))
 
 # Check that input data exist
 print("Checking that input data files exist")
@@ -21,4 +23,4 @@ assertthat::assert_that(file.exists(ul_prdata_file))
 
 # run SVD 
 out_file <- sprintf("%s/M_reduced_%d.RData", out_intermediates_dir, opt$svd_dimensions)
-ul_save_features_reduced_data_set(ul_file = ul_prdata_file, out_file = ul_reduced_prdata_file, svd_k = opt$svd_dimensions)
+ul_save_features_reduced_data_set(ul_file = ul_prdata_file, out_file = out_file, svd_k = opt$svd_dimensions, varimax_rotate = opt$apply_varimax)
