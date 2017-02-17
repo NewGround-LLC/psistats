5977e58b319e2b9aa0c35d7fade2b69c40e496d3
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-02-17 19:02:12 +0200

    adjusted parameters

--- eval_dnn.sh
+++ eval_dnn.sh
@@ -5,7 +5,7 @@
 
 ~/anaconda3/bin/Rscript src/nn_analysis.R --learning_rate 0.0001 \
                                           --max_steps 50000 \
-                                          --layers 512,256 \
+                                          --layers 1024,512 \
                                           --batch_size 100 \
                                           --dropout 0.5 \
                                           --lr_anneal_step 10000 \
