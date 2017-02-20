6ef7b44c1b1bbb6efecea9a4fc49254e62e68659
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-02-20 19:07:46 +0200

    Changed learning parameters

--- eval_dnn.sh
+++ eval_dnn.sh
@@ -3,11 +3,12 @@
 # The evaluation script for experiment with Deep Learning Network
 #
 
-~/anaconda3/bin/Rscript src/nn_analysis.R --learning_rate 0.001 \
+~/anaconda3/bin/Rscript src/nn_analysis.R --learning_rate 0.00001 \
                                           --max_steps 30000 \
-                                          --layers 2048,1024 \
+                                          --layers 512,256 \
                                           --batch_size 100 \
                                           --dropout 0.5 \
                                           --lr_anneal_step 10000 \
+                                          --lr_decay_rate 0.96 \
                                           --network_type dnn \
                                           --data_features_file $1
