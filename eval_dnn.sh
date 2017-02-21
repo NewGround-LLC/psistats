ae4836734262371a593d71b67fb5ab57cab2be43
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-02-21 19:31:42 +0200

    Fixed parameters

--- eval_dnn.sh
+++ eval_dnn.sh
@@ -3,9 +3,9 @@
 # The evaluation script for experiment with Deep Learning Network
 #
 
-~/anaconda3/bin/Rscript src/nn_analysis.R --learning_rate 0.00001 \
-                                          --max_steps 30000 \
-                                          --layers 512,256 \
+~/anaconda3/bin/Rscript src/nn_analysis.R --learning_rate 0.0001 \
+                                          --max_steps 50000 \
+                                          --layers 1536,512 \
                                           --batch_size 100 \
                                           --dropout 0.5 \
                                           --lr_anneal_step 10000 \
