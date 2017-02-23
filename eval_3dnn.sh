6b8edc79d815b02e1b20573a39c4ecc11a419adc
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-02-23 17:36:58 +0200

    Added results for batch size 10 and three hidden layer dnn

--- eval_3dnn.sh
+++ eval_3dnn.sh
@@ -6,7 +6,7 @@
 ~/anaconda3/bin/Rscript src/nn_analysis.R --learning_rate 0.0001 \
                                           --max_steps 50000 \
                                           --layers 1024,512,512 \
-                                          --batch_size 100 \
+                                          --batch_size 10 \
                                           --dropout 0.5 \
                                           --lr_anneal_step 10000 \
                                           --lr_decay_rate 0.96 \
