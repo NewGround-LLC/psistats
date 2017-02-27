d6288a88ea1c5ebae56c99581975eccd22316ca2
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-02-27 19:00:40 +0200

    Parameters adjustment

--- eval_3dnn.sh
+++ eval_3dnn.sh
@@ -5,8 +5,8 @@
 
 ~/anaconda3/bin/Rscript src/nn_analysis.R --learning_rate 0.0001 \
                                           --max_steps 50000 \
-                                          --layers 340,200,200 \
-                                          --batch_size 10 \
+                                          --layers 2048,1024,1024 \
+                                          --batch_size 100 \
                                           --dropout 0.5 \
                                           --lr_anneal_step 10000 \
                                           --lr_decay_rate 0.96 \
